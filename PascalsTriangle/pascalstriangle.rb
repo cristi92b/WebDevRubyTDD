9c6b3225de25653a4c775eff6a562c3cc427b16e
Razvan Deaconesu <student@mjolnir.labs.cs.pub.ro>
2013-11-02 12:24:40 +0200

    Pascal's triangle working

--- PascalsTriangle/pascalstriangle.rb
+++ PascalsTriangle/pascalstriangle.rb
@@ -1,4 +1,36 @@
-def compute_row(n)
-  []
+#!/usr/bin/env ruby
+
+def factorial(x)
+	i=0
+	t=1
+	p=1
+	while i<x
+		p=p*(t+i)
+		i=i+1
+	end
+	return p	
 end
+
+def c(n,k)
+	return (factorial(n))/(factorial(n-k)*factorial(k))
+end
+
+def compute_row(n)
+	if n.class.eql?(Fixnum)
+		t=0
+		i=0
+		a=[]
+		while t==0
+			a << c(n-1,i)
+			i=i+1
+			if(i>n-1)
+				t=1;
+			end
+		end
+		return a
+	else
+		return nil
+	end
+end
+
 
